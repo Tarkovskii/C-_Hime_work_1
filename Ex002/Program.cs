fd995b34de53c8909d38e2644457ed57614504f4
Daniil <trombonil1996@gmail.com>
2022-08-08 10:19:51 +0300

    change

---
+++ Ex002/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberSec = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberThir = Convert.ToInt32(Console.ReadLine());
+
+int max = numberOne;
+
+if (numberOne > numberSec)
+{
+    max = numberOne;
+}
+else
+{
+    max = numberSec;
+}
+
+if (numberThir > max)
+{
+    max = numberThir;
+}
+
+
+Console.Write("Большее число из трёх = ");
+Console.WriteLine(max);
+
