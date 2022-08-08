0f1b11d3fce2ed05d54cd2dc5ebcc5b663240821
Daniil <trombonil1996@gmail.com>
2022-08-09 00:14:34 +0300

    done_ex004

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Введите число.");
+double number = Convert.ToInt32(Console.ReadLine());
+
+double numberOne = 1;
+
+if (number == 1)
+    {
+        Console.Write("Лучше начинать с цифры ");
+    }
+
+
+if (number % 2 == 0)
+    {
+        while (numberOne <= number - 2 )
+        {
+            if ( numberOne % 2 == 0)
+                {
+                    Console.Write($"{numberOne}, ");
+                }
+
+            numberOne++;
+        }
+
+
+        if (numberOne % 2 != 0)
+            {
+                Console.Write($"{numberOne + 1}.");
+            }
+
+    }
+
+
+else
+    {
+        while (numberOne < number - 2 )
+            {
+                if ( numberOne % 2 == 0 )
+                    {
+                        Console.Write($"{numberOne}, ");
+                    }
+
+                        numberOne++;
+            }
+
+        if (numberOne % 2 != 0 )
+            {
+                Console.Write($"{numberOne + 1}.");
+            }
+    }
+
+
