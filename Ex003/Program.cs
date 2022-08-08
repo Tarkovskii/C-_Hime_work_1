526ca7f4c8062776f50f70f1aab71ee9f08249d5
Daniil <trombonil1996@gmail.com>
2022-08-08 10:39:44 +0300

    done_Ex003

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Введите число");
+double numberRand = Convert.ToInt32(Console.ReadLine());
+
+double result = (numberRand % 2);
+
+if (result == 0)
+{
+    Console.Write("Число ");
+    Console.Write(numberRand);
+    Console.Write(" - чётное.");
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(numberRand);
+    Console.Write(" - нечётное.");
+}
