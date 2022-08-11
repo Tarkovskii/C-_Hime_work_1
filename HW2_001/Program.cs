3a0be2a49592775cce2634d6136071b98047efd5
Daniil <trombonil1996@gmail.com>
2022-08-11 20:55:35 +0300

    HW001_done

--- HW2_001/Program.cs
+++ HW2_001/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ShowSecNumber(int number)
+{
+int numberFirst = number / 100;
+int numberSec = (number - (numberFirst * 100)) / 10;
+int numberThr = number % 10;
+
+Console.WriteLine(number);
+Console.WriteLine($"Вторая цифра этого числа - {numberSec}");
+}
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+// Random random = new Random();
+// int number = random.Next(100, 1000);
+
+ShowSecNumber(number);
