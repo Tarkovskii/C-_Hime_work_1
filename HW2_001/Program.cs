debcc1a1c97a268a69fbd39f6d5318718ed623b6
Daniil <trombonil1996@gmail.com>
2022-08-14 08:55:42 +0300

    mod

--- HW2_001/Program.cs
+++ HW2_001/Program.cs
@@ -2,12 +2,12 @@
 Console.WriteLine("Введите трёхзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int ShowThrNumber(int number)
+int ShowSecNumber(int number)
 {
     int numberFirst = number / 100;
     int numberSec = (number - (numberFirst * 100)) / 10;
     int numberThr = number % 10;
     return numberSec;
 }
-int number2 = ShowThrNumber(number);
+int number2 = ShowSecNumber(number);
 Console.WriteLine(number2);
