6399a1cb02d00d9aa0132c8cf598b0623dab5ca9
Daniil <trombonil1996@gmail.com>
2022-08-14 08:50:36 +0300

    mod

--- HW2_001/Program.cs
+++ HW2_001/Program.cs
@@ -1,16 +1,13 @@
-﻿void ShowSecNumber(int number)
-{
-int numberFirst = number / 100;
-int numberSec = (number - (numberFirst * 100)) / 10;
-int numberThr = number % 10;
-
-Console.WriteLine(number);
-Console.WriteLine($"Вторая цифра этого числа - {numberSec}");
-}
-
+﻿
 Console.WriteLine("Введите трёхзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
-// Random random = new Random();
-// int number = random.Next(100, 1000);
 
-ShowSecNumber(number);
+int ShowSecNumber(int number)
+{
+    int numberFirst = number / 100;
+    int numberSec = (number - (numberFirst * 100)) / 10;
+    int numberThr = number % 10;
+    return numberSec;
+}
+int number2 = ShowSecNumber(number);
+Console.WriteLine(number2);
