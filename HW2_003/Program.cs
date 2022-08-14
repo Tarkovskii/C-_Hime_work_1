a2632662c73952a04edfd23209ee7bc4330275db
Daniil <trombonil1996@gmail.com>
2022-08-14 18:47:04 +0300

    Hw003_done

--- HW2_003/Program.cs
+++ HW2_003/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер число");
+int numDay = Convert.ToInt32(Console.ReadLine());
+
+bool CheckWeekend(int number)
+{
+    if (number == 6 | number == 7)
+    {
+        return true;
+    }
+
+    if (number >= 1 & number < 7 /*& number != 6 & number != 7*/)
+    {
+        return false;
+    }
+
+    throw new Exception("Только от 1 до 7");
+
+}
+
+bool result = CheckWeekend(numDay);
+
+if (result == true)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
