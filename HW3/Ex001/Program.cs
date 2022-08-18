8ced6686e041a9052f31fbd5d1409b786f2410c7
Daniil <trombonil1996@gmail.com>
2022-08-18 17:41:44 +0300

    mod

--- HW3/Ex001/Program.cs
+++ HW3/Ex001/Program.cs
@@ -1,35 +1,45 @@
 ﻿Console.WriteLine("Введите пятизначное число");
-int number = Convert.ToInt32(Console.ReadLine());
+string number = Console.ReadLine();
 
-int CheckPalindrome(int number)
+void CheckPalindrome(string number)
 {
-    string numStr = $"{number}";
-    int numLen = numStr.Length;
 
-    string resStrFirst = numStr[0].ToString();
-    int numFirst = int.Parse(resStrFirst);
-    string resStrSec = numStr[1].ToString();
-    int numSec = int.Parse(resStrSec);
-    string resStrFour = numStr[3].ToString();
-    int numFour = int.Parse(resStrFour);
-    string resStrFift = numStr[4].ToString();
-    int numFift = int.Parse(resStrFift);
+    int numLen = number.Length;
 
-    if (numFirst == numFift & numSec == numFour)
+    // char[] newStr = new char[0];
+
+    for (int i = numLen; i >= 0; i--)
     {
-        return 1;
+        char[] newStr = new char[numLen];
+        Console.Write(newStr[i]);
     }
-    return 0;
+
+
+
+    // string resStrFirst = numStr[0].ToString();
+    // int numFirst = int.Parse(resStrFirst);
+    // string resStrSec = numStr[1].ToString();
+    // int numSec = int.Parse(resStrSec);
+    // string resStrFour = numStr[3].ToString();
+    // int numFour = int.Parse(resStrFour);
+    // string resStrFift = numStr[4].ToString();
+    // int numFift = int.Parse(resStrFift);
+
+    // if (numFirst == numFift & numSec == numFour)
+    // {
+    //     return 1;
+    // }
+    // return 0;
 
 }
 
-int result = CheckPalindrome(number);
+// int result = CheckPalindrome(number);
 
-if (result == 1)
-{
-    Console.WriteLine($"Число {number} - палиндром.");
-}
-else
-{
-    Console.WriteLine($"Число {number} - не палиндром.");
-}
+// if (result == 1)
+// {
+//     Console.WriteLine($"Число {number} - палиндром.");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {number} - не палиндром.");
+// }
--- HW3/Ex002/Program.cs
+++ HW3/Ex002/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите координаты первой точки.");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Введите координаты второй точки.");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double SserchSizeLin(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+}
+
+double rezult = SserchSizeLin(x1, y1, z1, x2, y2, z2);
+Console.WriteLine($"Длина отрезка = {rezult}");
