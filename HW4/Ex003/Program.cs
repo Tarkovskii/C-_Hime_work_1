adaed5a624666430672cfc609367fb3d83e3eec5
Daniil <trombonil1996@gmail.com>
2022-08-23 09:16:12 +0300

    Ex003done

--- HW4/Ex003/Program.cs
+++ HW4/Ex003/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int[8];
+
+int[] FilArray(int[] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(0, 100);
+    }
+    return arr;
+}
+
+string PrintArray(int[] arr)
+{
+    string result = " ";
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result += $"{arr[i]},";
+    }
+    return result;
+}
+
+string strLine = PrintArray(FilArray(array));
+Console.WriteLine(strLine);
