4247103f9855ce156d4fc008e1922d37d9e675a1
Daniil <trombonil1996@gmail.com>
2022-08-23 08:41:37 +0300

    Ex02Done

---
+++ HW4/Ex002/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите число");
+string numb = Console.ReadLine();
+
+int[] FillArray(string numb)
+{
+    string numS = numb[0].ToString();
+    int numAr = int.Parse(numS);
+
+    int[] arr = new int[numb.Length];
+
+    for (int i = 0; i < numb.Length; i++)
+    {
+        numS = numb[i].ToString();
+        numAr = int.Parse(numS);
+        arr[i] = numAr;
+    }
+    return arr;
+}
+
+void SumArray(int[] arr)
+{
+    int numStr = 0;
+    int sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum = arr[i];
+
+        numStr += sum;
+
+    }
+
+    Console.WriteLine(numStr);
+}
+
+SumArray(FillArray(numb));
