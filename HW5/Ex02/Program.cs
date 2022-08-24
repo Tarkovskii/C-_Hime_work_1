8420355d0df62a69c7f1518429961aed97f26094
Daniil <trombonil1996@gmail.com>
2022-08-24 19:32:17 +0300

    Ex02_done

---
+++ HW5/Ex02/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Сколько чисел вывести?");
+int count = Convert.ToInt32(Console. ReadLine());
+
+int[] FillArray(int count)
+{
+    int[] arr = new int[count];
+
+    Random random = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(-99, 100);
+    }
+    return arr;
+}
+
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int GetIntSumOddPos(int[] arr)
+{
+    int sumNum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            sumNum += arr[i];
+        }
+    }
+    return sumNum;
+}
+Console.WriteLine("");
+
+int[] arr = FillArray(count);
+PrintArray(arr);
+int result = GetIntSumOddPos(arr);
+Console.WriteLine($" -> {result}");
