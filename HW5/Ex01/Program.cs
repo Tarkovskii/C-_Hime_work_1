5410d8f56e4b67ff8b5dbf398bfbd8281d87799e
Daniil <trombonil1996@gmail.com>
2022-08-24 18:57:33 +0300

    Ex001_done

--- HW5/Ex01/Program.cs
+++ HW5/Ex01/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Сколько чисел вывести?");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] FillArray(int number)
+{
+    int[] arr = new int[number];
+    Random random = new Random();
+
+    for (int i = 0; i < number; i++)
+    {
+        arr[i] = random.Next(100, 1000);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int CountEvenArray(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+
+}
+
+Console.WriteLine("");
+int[] arr = FillArray(number);
+PrintArray(arr);
+int countEven = CountEvenArray(arr);
+Console.WriteLine($" количество чётных чисел - {countEven}");
