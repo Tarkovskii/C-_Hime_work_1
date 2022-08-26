d9a5f4b589a15d5017c4266a578298bc2df379c6
Daniil <trombonil1996@gmail.com>
2022-08-26 19:44:32 +0300

    Ex03Done

---
+++ HW5/Ex03/Program.cs
@@ -0,0 +1,85 @@
+﻿Console.WriteLine("Сколько чисел вывести?");
+int numb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+float[] FillArray(int numb)
+{
+    Random a = new Random();
+    Random b = new Random();
+
+    float[] arr = new float[numb];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int an = a.Next(-10, 100);
+        int bn = b.Next(-10, 11);
+        float f = an;
+        float c = bn;
+
+        if (c == 0)
+        {
+            arr[i] = 0;
+        }
+        else
+        {
+            float res = f / c;
+            arr[i] = res;
+        }
+    }
+    return arr;
+}
+
+
+void PrintArray(float[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+float GetMaxValue(float[] arr)
+{
+    float maxNum = arr[0];
+
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (maxNum < arr[i])
+        {
+            maxNum = arr[i];
+        }
+
+    }
+    return maxNum;
+}
+
+float GetMinValue(float[] arr)
+{
+    float minA = arr[0];
+
+    int i = 0;
+
+    while (i < arr.Length)
+    {
+        if (minA > arr[i])
+        {
+            minA = arr[i];
+            i++;
+        }
+        else
+        {
+            i++;
+        }
+
+    }
+    return minA;
+}
+
+
+float[] arr = FillArray(numb);
+float max = GetMaxValue(arr);
+float min = GetMinValue(arr);
+PrintArray(arr);
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine($"Разница между большим числом - {max} и меньшим числом - {min} равняется -  {max - min}");
