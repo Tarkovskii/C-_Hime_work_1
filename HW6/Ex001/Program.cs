32c9210cfe469f53c344900a96e3aeb72fed36cc
Daniil <trombonil1996@gmail.com>
2022-08-30 19:57:28 +0300

    hw6

---
+++ HW6/Ex001/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Сколько чиcел хотите ввести?");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] FillArray(int num)
+{
+    int[] arr = new int[num];
+
+    for (int i = 0; i < num; i++)
+    {
+        // Console.WriteLine("Введите число.");
+        int value = Convert.ToInt32(Console.ReadLine());
+
+        arr[i] = value;
+    }
+    return arr;
+}
+Console.WriteLine("");
+
+int GerCountEvenNum(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i <arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] arr = FillArray(num);
+int countRes = GerCountEvenNum(arr);
+Console.WriteLine("");
+PrintArray(arr);
+Console.WriteLine($"-> {countRes}");
---
+++ HW6/Ex002/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите первую точку");
+float b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите вторую точку");
+float k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третью точку");
+float b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите четвёртую точку");
+float k2 = Convert.ToInt32(Console.ReadLine());
+
+
+float GetXlocation(float b1, float k1, float b2, float k2)
+{
+    float x = ((b1 * (-1)) + b2) / (k1 - k2);
+
+    return x;
+}
+
+float GetYlocation(float b2, float k2, float x)
+{
+    float y = (k2 * x) + b2;
+
+    return y;
+}
+
+float x = GetXlocation(b1, k1, b2, k2);
+float y = GetYlocation(b2, k2, x);
+
+Console.WriteLine($"x = {x}, y = {y}");
