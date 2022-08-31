74a1d867f212ad18fccc102517836035b07377f9
Daniil <trombonil1996@gmail.com>
2022-08-31 10:40:19 +0300

    ex002_check

--- HW6/Ex002/Program.cs
+++ HW6/Ex002/Program.cs
@@ -1,28 +1,32 @@
-﻿Console.WriteLine("Введите первую точку");
-float b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите вторую точку");
-float k1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите третью точку");
-float b2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите четвёртую точку");
-float k2 = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Введите первый параметр первой функции.");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второй параметр первой функции.");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите первый параметр второй функции.");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второй параметр второй функции.");
+double k2 = Convert.ToDouble(Console.ReadLine());
 
-
-float GetXlocation(float b1, float k1, float b2, float k2)
+if (k1==k2)
 {
-    float x = ((b1 * (-1)) + b2) / (k1 - k2);
+    Console.WriteLine("Графики параллельны");
+}
+
+double GetXlocation(double b1, double k1, double b2, double k2)
+{
+    double x = ((b1*(-1)) + b2) / (k1 - k2);
 
     return x;
 }
 
-float GetYlocation(float b2, float k2, float x)
+double GetYlocation(double b2, double k2, double x)
 {
-    float y = (k2 * x) + b2;
+   double y = (k2 * x) + b2;
 
     return y;
 }
 
-float x = GetXlocation(b1, k1, b2, k2);
-float y = GetYlocation(b2, k2, x);
+double x = GetXlocation(b1, k1, b2, k2);
+double y = GetYlocation(b2, k2, x);
 
 Console.WriteLine($"x = {x}, y = {y}");
